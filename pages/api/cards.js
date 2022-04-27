e14e4546aa613e86dd926b65b6a6eea8406d502b
Franzi Graml <franzi.graml@gmx.de>
2022-04-27 16:48:24 +0200

    Add comment

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -3,11 +3,11 @@ import { getCards } from "../../src/services/get-cards";
 export default function handler(request, response) {
   const cards = getCards();
 
-  cards.push({
+  /* cards.push({
     id: 1,
     content: "Testy test",
     name: "Testname",
-  });
+  }); */
 
   response.status(200).json(cards);
 }
